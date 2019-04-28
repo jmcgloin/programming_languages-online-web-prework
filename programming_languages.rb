7808b0882dea98de3198e7644b6192bcc6fba5b2
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-28 03:08:27 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,6 +2,6 @@ require 'pry'
 
 def reformat_languages(languages)
   num_styles = languages.length
-  num_langs_per_style = languages.values.count
+  num_langs_per_style = languages.flatten.length - num_styles
   binding.pry
 end
