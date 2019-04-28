c90a37244611d9f3e1e0bbb2bce48f619ae8c7f3
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-28 03:35:49 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,5 +2,11 @@ require 'pry'
 
 def reformat_languages(languages)
   language_hash = Hash.new
+  styles = languages.keys
+  style.each do
+    |style|
+    languages[style].keys.each do
+      |lang|
+      
   binding.pry
 end
