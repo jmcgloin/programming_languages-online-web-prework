5224107dc3f150075f867566a3900c92e3527c7d
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-28 03:31:07 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,7 +5,7 @@ def reformat_languages(languages)
   languages.values.each do
     |lang|
     language_hash[lang] = {
-      :type => lang.val,
+      :type => lang.values,
       :style => languages.key(lang)
     }
   end
